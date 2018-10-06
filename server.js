dbc961587c2e86df0eee264582455e453875f8cb
David Vaca <david1986@gmail.com>
2018-10-06 17:07:49 -0500

    serve mejorado

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/registro-eventos'));
-app.use('/js', express.static(__dirname + '/dist/js'));
-app.use('/css', express.static(__dirname + '/dist/css'));
 
-app.get('/*', function(req,res) {
+//app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
+//res.sendFile(path.join(__dirname+'/dist/index.html'));
+//});
+res.use("/", express.static(__dirname+'/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
