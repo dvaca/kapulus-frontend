5311bf625c27d7fefbd1be56acaf1c2ceb97aa67
David Vaca <david1986@gmail.com>
2018-10-11 17:49:26 -0500

    Publicacion call center

--- server.js
+++ server.js
@@ -7,15 +7,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/registro-eventos'));
 app.use("/", express.static(__dirname+'/dist'));
+app.use("/callcenter_token_7D5H3N9Y4F", express.static(__dirname+'/dist'));
+app.use("/registro", express.static(__dirname+'/dist'));
 
 //app.get('/*', function(req,res) {
     
 //res.sendFile(path.join(__dirname+'/dist/index.html'));
 //});
-
-app.get('/callcenter_token_7D5H3N9Y4F', function(req,res) {    
-	res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
 
 
 // Start the app by listening on the default Heroku port
