c945bc39f59c5f9b7a582ad675d00a9b11d000b4
David Vaca <david1986@gmail.com>
2019-05-31 01:12:47 -0500

    Estadisticas evento Coosalud Bmanga

--- src/app/estadisticas-local/estadisticas-local.component.ts
+++ src/app/estadisticas-local/estadisticas-local.component.ts
@@ -27,6 +27,9 @@ export class EstadisticasLocalComponent implements OnInit {
 	if(url == 'evento_estadisticas_coosalud'){
       this.cargarEvento(12, 11);
     }
+	if(url == 'evento_estadisticas_coosalud_bmanga'){
+      this.cargarEvento(14, 13);
+    }
 	if(url == 'evento_estadisticas_coosalud_medellin'){
       this.cargarEvento(17, 16);
     }
