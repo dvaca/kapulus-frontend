83ff6a7b021e20743d4c4dc385a391294b7f0202
dvaca <43690454+dvaca@users.noreply.github.com>
2019-11-18 00:02:53 -0500

    Registro credibanco

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { LoginComponent } from './login/login.component';
 import { EstadisticasLocalComponent } from './estadisticas-local/estadisticas-local.component';
 import { CertificadoComponent } from './certificado/certificado.component';
 import { DetalleAsistenteComponent } from './detalle-asistente/detalle-asistente.component';
-import { RegistroCredibancoComponent } from './registro-credibanco/registro-credibanco';
+import { RegistroCredibancoComponent } from './registro-credibanco/registro-credibanco.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/registro-online', pathMatch: 'full' },
