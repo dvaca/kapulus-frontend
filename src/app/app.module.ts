bd59e92fd867b671e4fd28bfe06abb5661e2c543
dvaca <43690454+dvaca@users.noreply.github.com>
2019-11-18 00:03:40 -0500

    Registro Credibanco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { RegistroCredibancoComponent } from './registro_credibanco/registro-credibanco';
+import { RegistroCredibancoComponent } from './registro_credibanco/registro-credibanco.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
