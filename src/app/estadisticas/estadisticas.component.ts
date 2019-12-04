4233ef7fe5ec3aa1ec88d20df605f140984c7b1e
dvaca <43690454+dvaca@users.noreply.github.com>
2019-12-03 22:39:02 -0500

    Montaje Semana Region Central

--- src/app/estadisticas/estadisticas.component.ts
+++ src/app/estadisticas/estadisticas.component.ts
@@ -164,8 +164,11 @@ export class EstadisticasComponent implements OnInit {
 	if(url == 'callcenter_estadisticas_energetico'){
       this.cargarEvento(56, 56);
     }
-  if(url == 'callcenter_registro_oracle_fin1'){
+  if(url == 'callcenter_estadisticas_oracle_fin1'){
       this.cargarEvento(64, 64);
+    }
+  if(url == 'callcenter_estadisticas_region_central'){
+      this.cargarEvento(63, 63);
     }
     
     this.cargarZonas();
