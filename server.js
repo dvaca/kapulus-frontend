46641cf6616a7c16557c039d3fda2801246574dc
dvaca <43690454+dvaca@users.noreply.github.com>
2021-01-12 19:41:56 -0500

    Evento RTVC colaboradores

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use("/registro-rio-bogota", express.static(__dirname+'/dist'));
 app.use("/registro-prosegur", express.static(__dirname+'/dist'));
 app.use("/registro-credibanco", express.static(__dirname+'/dist'));
 app.use("/registro-andi-procultivos", express.static(__dirname+'/dist'));
+app.use("/registro-rtvc-colaboradores", express.static(__dirname+'/dist'));
 
 app.use("/callcenter_registro_cerveza", express.static(__dirname+'/dist'));
 app.use("/callcenter_registro_orquideas", express.static(__dirname+'/dist'));
@@ -85,6 +86,7 @@ app.use("/callcenter_registro_canal_digital", express.static(__dirname+'/dist'))
 app.use("/callcenter_registro_amarilo", express.static(__dirname+'/dist'));
 app.use("/callcenter_registro_andi", express.static(__dirname+'/dist'));
 app.use("/callcenter_registro_rtvc_usuarios", express.static(__dirname+'/dist'));
+app.use("/callcenter_registro_rtvc_colaboradores", express.static(__dirname+'/dist'));
 
 app.use("/callcenter_estadisticas_coosalud_bquilla", express.static(__dirname+'/dist'));
 app.use("/callcenter_estadisticas_coosalud_bmanga", express.static(__dirname+'/dist'));
@@ -134,6 +136,7 @@ app.use("/callcenter_estadisticas_canal_digital", express.static(__dirname+'/dis
 app.use("/callcenter_estadisticas_amarilo", express.static(__dirname+'/dist'));
 app.use("/callcenter_estadisticas_andi", express.static(__dirname+'/dist'));
 app.use("/callcenter_estadisticas_rtvc_usuarios", express.static(__dirname+'/dist'));
+app.use("/callcenter_estadisticas_rtvc_colaboradores", express.static(__dirname+'/dist'));
 
 app.use("/evento_estadisticas_semana", express.static(__dirname+'/dist'));
 app.use("/evento_estadisticas_coosalud", express.static(__dirname+'/dist'));
